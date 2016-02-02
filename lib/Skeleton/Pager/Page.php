1524de97f530f4d95538abd2fb5591a784853cbf
Christophe Gosiau <christophe@tigron.be>
2016-02-02 13:26:53 +0100

    rewrite query so that DISTINCT, ORDER BY and LIMIT are not combined, use of subquery instead

--- lib/Skeleton/Pager/Page.php
+++ lib/Skeleton/Pager/Page.php
@@ -60,8 +60,8 @@ trait Page {
 		if (strtolower($direction) != 'asc') {
 			$direction = 'desc';
 		}
-
-		$sql = 'SELECT DISTINCT(' . $table . '.' . self::trait_get_table_field_id() . ') ' . "\n";
+		$sql = 'SELECT ' . $table . '.' . self::trait_get_table_field_id() . ' FROM ' . $table . ' WHERE ' . self::trait_get_table_field_id() . ' IN ( ' . "\n";
+		$sql .= 'SELECT ' . $table . '.' . self::trait_get_table_field_id() . "\n";
 		$sql .= 'FROM `' . $table . '`' . "\n";
 
 		/**
@@ -138,6 +138,7 @@ trait Page {
 
 			$sql .= 'ORDER BY ' . $sort . ' ' . $direction;
 		}
+		$sql .= "\n" . ')' . "\n";
 
 		if ($all !== true AND $sorter == 'db') {
 			$sql .= ' LIMIT ' . ($page-1)*$limit . ', ' . $limit;
